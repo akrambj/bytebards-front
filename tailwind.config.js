d3ac125c180304965a3e2618ad3790c0f1b23f7d
akrambj <bedjaouiakram.93@gmail.com>
2024-03-01 14:25:46 +0000

    auth

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./node_modules/flowbite/**/*.js"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "samsung-sharp-sans": ["SamsungSharpSans", "sans-serif"],
+      },
+      colors: {
+        Green100: "#66DC90",
+        Gray66: "#A6BBD1",
+        Gray100: "#476788",
+        Typo: "#0B3558",
+        Blue100: "#006BFF",
+        Gray10: "#F8F9FB",
+        Blue10: "#F3FAFF",
+        cardBg: "#FCFCFD",
+      },
+      dropShadow: {
+        card: [
+          "0 26px 93px rgba(0, 0, 0, 0.0)",
+          "0 24px 60px rgba(0, 0, 0, 0.0)",
+          "0 20px 34px rgba(0, 0, 0, 0.01)",
+          "0 15px 15px rgba(0, 0, 0, 0.02)",
+          "0 8px 4px rgba(0, 0, 0, 0.02)",
+        ],
+      },
+    },
   },
   plugins: [],
 };
